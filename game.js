b3d46f04693a6a0fe86b1aca207017722ca0aa55
Mike Santry <santrym@xavier.edu>
2018-11-02 12:39:57 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,22 @@
+// Initialize the Phaser Game object and set default game window size
+const game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+  preload: preload,
+  create: create,
+  update: update })
+
+// Declare shared variables at the top so all methods can access them
+
+
+function preload () {
+  // Load & Define our game assets
+  
+}
+
+function create () {
+
+}
+
+function update () {
+ 
+}
+
